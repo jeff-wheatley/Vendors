b546f88852cf871250b8ba1f041deeae962e90b2
Jeff Wheatley <jwheatley@triu.com>
2018-04-02 11:53:26 -0400

    (missed some files> First functional draft

--- vendors/grails-app/init/com/hcs/BootStrap.groovy
+++ vendors/grails-app/init/com/hcs/BootStrap.groovy
@@ -1,17 +1,35 @@
 package com.hcs
 
 import org.joda.time.LocalDate
+import grails.util.Environment
 
 class BootStrap {
 
     def init = { servletContext ->
-        def lows = new Vendor(name: 'Lowes').save()
-        def cosco = new Vendor(name: 'Cosco').save()
+        if(Environment.current == Environment.DEVELOPMENT) {
+            LocalDate date = LocalDate.parse("2018-03-25")
 
-        new Expense(day: LocalDate.now(), amount:95.13, vendor: lows).save()
-        new Expense(day: LocalDate.now(), amount:99.86, vendor: cosco).save()
-        new Sale(day: LocalDate.now(), amount:215.17).save()
+            // Create some reference vendors and commissionVendors
+            def sams = new Vendor(name: 'Sams').save(flush: true)
+            def cosco = new Vendor(name: 'Cosco').save(flush: true)
+            def pepsi = new CommissionVendor(name: 'Pepsi').save(flush: true)
+            def coke = new CommissionVendor(name: 'Coke').save(flush: true)
+
+            // create some expenses and operationalExpenses
+            new CostOfSale(dateOfCosts: date, amount: 123.45, vendor: sams).save(flush: true)
+            new CostOfSale(dateOfCost: date, amount: 242.88, vendor: cosco).save(flush: true)
+            new OperationalExpense(dateOfExpense: date, amount: 123.45, type: OperationalExpenseType.JANITORIAL).save(flush: true)
+            new OperationalExpense(dateOfExpense: date, amount: 223.99, type: OperationalExpenseType.OFFICE).save(flush: true)
+
+            // Create some sales and Commissions
+            new Sale(dateOfSale: date, amount: 123.45).save(flush: true)
+            new Sale(dateOfSale: date, amount: 255.99).save(flush: true)
+            new Commission(dateOfCommission: date, amount: 123.45, commissionVendor: pepsi).save(flush: true)
+            new Commission(dateOfCommission: date, amount: 151.50, commissionVendor: coke).save(flush: true)
+        }
+
     }
+
     def destroy = {
     }
 }
--- vendors/grails-app/migrations/changelog.groovy
+++ vendors/grails-app/migrations/changelog.groovy
@@ -1,66 +1,148 @@
 databaseChangeLog = {
 
-    changeSet(author: "jwheatley (generated)", id: "1522172260692-1") {
-        createTable(tableName: "expense") {
-            column(autoIncrement: "true", name: "id", type: "BIGINT") {
-                constraints(primaryKey: "true", primaryKeyName: "expensePK")
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-1") {
+        createTable(tableName: "COMMISSION") {
+            column(autoIncrement: "true", name: "ID", type: "BIGINT(19)") {
+                constraints(primaryKey: "true", primaryKeyName: "CONSTRAINT_5")
             }
 
-            column(name: "version", type: "BIGINT") {
+            column(name: "VERSION", type: "BIGINT(19)") {
                 constraints(nullable: "false")
             }
 
-            column(name: "day_of_expense", type: "BLOB(255)") {
+            column(name: "DAY_OF_COMMISSION", type: "VARBINARY(255)") {
                 constraints(nullable: "false")
             }
 
-            column(name: "vendor_id", type: "BIGINT") {
+            column(name: "AMOUNT", type: "DECIMAL(19, 2)") {
                 constraints(nullable: "false")
             }
 
-            column(name: "amount", type: "DECIMAL(19, 2)") {
-                constraints(nullable: "false")
-            }
-        }
-    }
-
-    changeSet(author: "jwheatley (generated)", id: "1522172260692-2") {
-        createTable(tableName: "sale") {
-            column(autoIncrement: "true", name: "id", type: "BIGINT") {
-                constraints(primaryKey: "true", primaryKeyName: "salePK")
-            }
-
-            column(name: "version", type: "BIGINT") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "day_of_sale", type: "BLOB(255)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "amount", type: "DECIMAL(19, 2)") {
+            column(name: "COMMISSION_VENDOR_ID", type: "BIGINT(19)") {
                 constraints(nullable: "false")
             }
         }
     }
 
-    changeSet(author: "jwheatley (generated)", id: "1522172260692-3") {
-        createTable(tableName: "vendor") {
-            column(autoIncrement: "true", name: "id", type: "BIGINT") {
-                constraints(primaryKey: "true", primaryKeyName: "vendorPK")
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-2") {
+        createTable(tableName: "COMMISSION_VENDOR") {
+            column(autoIncrement: "true", name: "ID", type: "BIGINT(19)") {
+                constraints(primaryKey: "true", primaryKeyName: "CONSTRAINT_C")
             }
 
-            column(name: "version", type: "BIGINT") {
+            column(name: "VERSION", type: "BIGINT(19)") {
                 constraints(nullable: "false")
             }
 
-            column(name: "name", type: "VARCHAR(40)") {
+            column(name: "NAME", type: "VARCHAR(40)") {
                 constraints(nullable: "false")
             }
         }
     }
 
-    changeSet(author: "jwheatley (generated)", id: "1522172260692-4") {
-        addForeignKeyConstraint(baseColumnNames: "vendor_id", baseTableName: "expense", constraintName: "FK5sibxu7jhjrfujed5s55cqfwd", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "vendor")
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-3") {
+        createTable(tableName: "COST_OF_SALE") {
+            column(autoIncrement: "true", name: "ID", type: "BIGINT(19)") {
+                constraints(primaryKey: "true", primaryKeyName: "CONSTRAINT_2")
+            }
+
+            column(name: "VERSION", type: "BIGINT(19)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "VENDOR_ID", type: "BIGINT(19)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "DAY_OF_COST", type: "VARBINARY(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "AMOUNT", type: "DECIMAL(19, 2)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-4") {
+        createTable(tableName: "OPERATIONAL_EXPENSE") {
+            column(autoIncrement: "true", name: "ID", type: "BIGINT(19)") {
+                constraints(primaryKey: "true", primaryKeyName: "CONSTRAINT_6")
+            }
+
+            column(name: "VERSION", type: "BIGINT(19)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "DAY_OF_EXPENSE", type: "VARBINARY(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "OPERATIONAL_EXPENSE_TYPE", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "AMOUNT", type: "DECIMAL(19, 2)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "DESCRIPTION", type: "VARCHAR(60)")
+        }
+    }
+
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-5") {
+        createTable(tableName: "SALE") {
+            column(autoIncrement: "true", name: "ID", type: "BIGINT(19)") {
+                constraints(primaryKey: "true", primaryKeyName: "CONSTRAINT_26")
+            }
+
+            column(name: "VERSION", type: "BIGINT(19)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "DAY_OF_SALE", type: "VARBINARY(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "AMOUNT", type: "DECIMAL(19, 2)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-6") {
+        createTable(tableName: "VENDOR") {
+            column(autoIncrement: "true", name: "ID", type: "BIGINT(19)") {
+                constraints(primaryKey: "true", primaryKeyName: "CONSTRAINT_9")
+            }
+
+            column(name: "VERSION", type: "BIGINT(19)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "NAME", type: "VARCHAR(40)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-7") {
+        createIndex(indexName: "FK6C49TT7P4CXHFT8XHRMVNLCGP_INDEX_5", tableName: "COMMISSION") {
+            column(name: "COMMISSION_VENDOR_ID")
+        }
+    }
+
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-8") {
+        createIndex(indexName: "FKRM0SBY4SS6HEVJ8AS1KJATELT_INDEX_2", tableName: "COST_OF_SALE") {
+            column(name: "VENDOR_ID")
+        }
+    }
+
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-9") {
+        addForeignKeyConstraint(baseColumnNames: "COMMISSION_VENDOR_ID", baseTableName: "COMMISSION", constraintName: "FK6C49TT7P4CXHFT8XHRMVNLCGP", deferrable: "false", initiallyDeferred: "false", onDelete: "RESTRICT", onUpdate: "RESTRICT", referencedColumnNames: "ID", referencedTableName: "COMMISSION_VENDOR")
+    }
+
+    changeSet(author: "jwheatley (generated)", id: "1522683669264-10") {
+        addForeignKeyConstraint(baseColumnNames: "VENDOR_ID", baseTableName: "COST_OF_SALE", constraintName: "FKRM0SBY4SS6HEVJ8AS1KJATELT", deferrable: "false", initiallyDeferred: "false", onDelete: "RESTRICT", onUpdate: "RESTRICT", referencedColumnNames: "ID", referencedTableName: "VENDOR")
     }
 }
